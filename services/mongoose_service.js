eff0a5dd66b51e6ddc7451a3f926a8e9451cb3a5
simon <simon@uwotech.com>
2019-04-06 02:23:45 +0800

    change mongodb to local

--- services/mongoose_service.js
+++ services/mongoose_service.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://quest:temp_quest@47.97.110.251:47203/question' // 问卷线上
-// const uri = 'mongodb://127.0.0.1:27017/questions' // 本地
+// const uri = 'mongodb://quest:temp_quest@47.97.110.251:47203/question' // 问卷线上
+const uri = 'mongodb://127.0.0.1:27017/questions' // 本地
 
 mongoose.Promise = global.Promise;
 

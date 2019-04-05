1884a27bb5ab80611fca9087249cedccd721e1f8
simon <simon@uwotech.com>
2019-04-06 03:32:21 +0800

    update m

---
+++ m/precache-manifest.4bf351589fa03c55388bd386f8c81211.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb939f64bda6c4d79936",
+    "url": "/static/js/main.6807c857.chunk.js"
+  },
+  {
+    "revision": "7cb8f4c3a1d63ef11dd5",
+    "url": "/static/js/2.bf4194f1.chunk.js"
+  },
+  {
+    "revision": "fb939f64bda6c4d79936",
+    "url": "/static/css/main.1f6c4608.chunk.css"
+  },
+  {
+    "revision": "d5fca9469bff939db32a7cbc3cf416c1",
+    "url": "/index.html"
+  }
+];
